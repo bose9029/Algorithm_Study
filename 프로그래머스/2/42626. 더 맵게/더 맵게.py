dbda9215bff2a2ac6adea2a39eba8bb4496ce1ac
Muyoung Cheong <61954102+bose9029@users.noreply.github.com>
2026-05-25 15:41:07 +0900

    [level 2] Title: 더 맵게, Time: 1197.35 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(scoville, K):
+
+    # 스코빌 변수를 힙 형태로 변환
+    heapq.heapify(scoville)
+
+    # 섞은 횟수는 0으로 지정 
+    count = 0
+
+    # 가장 작은 스코빌 지수가 K보다 작을 동안 반복
+    while scoville[0] < K:
+
+        # 음식이 1개뿐이면 더 이상 섞을 수 없으므로 실패
+        if len(scoville) < 2:
+            return -1
+
+        # 가장 작은 두 음식을 꺼내 새로운 음식 생성 후 다시 넣기
+        heapq.heappush(
+            scoville,
+            heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        )
+
+        # 섞은 횟수 증가
+        count += 1
+
+    # 모든 음식의 스코빌 지수가 K 이상이면 횟수 반환
+    return count
