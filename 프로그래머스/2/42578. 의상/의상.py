3b5587343b92c46d540ab62ebe6fe40c055b53c0
Muyoung Cheong <61954102+bose9029@users.noreply.github.com>
2026-05-25 15:56:38 +0900

    코드 별 주석 수정

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,14 @@
 def solution(clothes):
+    # 종류별 옷 개수를 저장하고 관리하기 위해 딕셔너리형태로 저장 
     clothes_dict = {}
 
     # 옷 종류별 개수 세기
     for name, kind in clothes:
+        # 해당 옷 종류가 이미 딕셔너리에 있으면 기존 개수에 1을 더함
+        # 새로운 옷 종류가 추가될시 0에서 시작해서 1을 더함
         clothes_dict[kind] = clothes_dict.get(kind, 0) + 1
 
+    #곱셉을 해야하기 때문에 초기 값은 1로 지정 
     answer = 1
 
     # 각 종류마다 선택 경우의 수 계산
