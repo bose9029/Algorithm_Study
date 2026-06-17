366ed1464c031e77e928bf8d677f7ee7bce7e87e
Muyoung Cheong <61954102+bose9029@users.noreply.github.com>
2026-06-17 15:48:03 +0900

    문제풀이 수정

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,18 @@
 def solution(word):
+    #알파벳 모음을 배열에 저장
     vowels = ['A', 'E', 'I', 'O', 'U']
-    weights = [781, 156, 31, 6, 1]
+
+    # 가중치 계산
+    weights = []
+    for i in range(5):
+        weight = 0
+        for j in range(5 - i):
+            weight += 5 ** j
+        weights.append(weight)
 
     answer = 0
 
-    for i in range(len(word)):
-        answer += vowels.index(word[i]) * weights[i] + 1
+    for i, ch in enumerate(word):
+        answer += vowels.index(ch) * weights[i] + 1
 
     return answer
